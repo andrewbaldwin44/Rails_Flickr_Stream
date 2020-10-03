3e4ed2676f6ab2f8bb384f3c1a65ea9a01394767
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-10-03 12:00:00 -0400

    Add error handling

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,8 +1,12 @@
 class HomepageController < ApplicationController
   def index
-    if params[:user_id]
-      flickr = Flickr.new ENV["flickr_key"], ENV["flickr_secret"]
-      @photos = flickr.photos.search(user_id: params[:user_id])
+    begin
+      if params[:user_id]
+        flickr = Flickr.new ENV["flickr_key"], ENV["flickr_secret"]
+        @photos = flickr.photos.search(user_id: params[:user_id])
+      end
+    rescue
+      flash.now[:warning] = "Invalid user."
     end
   end
 end
