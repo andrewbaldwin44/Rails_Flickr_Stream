1b56666a604acd678b28cb0162ad94902991d7ed
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-10-03 11:40:38 -0400

    Get photostream from flickr

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,8 @@
 class HomepageController < ApplicationController
   def index
+    if params[:user_id]
+      flickr = Flickr.new ENV["flickr_key"], ENV["flickr_secret"]
+      @photos = flickr.photos.search(user_id: params[:user_id])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root 'homepage#index'
-
-  get '/flickr', to: 'flickr#index'
-  get '/flickr/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
